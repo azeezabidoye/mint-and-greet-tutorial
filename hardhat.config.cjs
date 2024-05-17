76e27f6678b3fb709959561e8eef92b677126ea9
azeezabidoye <azeezabidoye@yahoo.com>
2024-05-17 16:13:21 +0100

    Alfajores testnet configured

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const INFURA_SEPOLIA_URL = process.env.INFURA_SEPOLIA_URL;
+const INFURA_ALFAJORES_URL = process.env.INFURA_ALFAJORES_URL;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -16,6 +17,11 @@ module.exports = {
       chainId: 11155111,
       accounts: [`0x${PRIVATE_KEY}`], // Insert private Key
       url: INFURA_SEPOLIA_URL, // Insert INFURA Sepolia URL
+    },
+    alfajores: {
+      url: INFURA_ALFAJORES_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 44787,
     },
   },
   namedAccounts: {
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import { ethers, BrowserProvider } from "ethers";
+import Greeter from "../artifacts/contracts/Greeter.sol/Greeter.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+// const greeterContractAddress = "0x72226da7160FC59252869638728D8F9ad671069b"; // Sepolia Testnet
+const greeterContractAddress = "0xDbAC04C29c43442F8A7F77860768B9bD48f620C0"; // Alfajores testnet
+// const greeterContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Local network
 
+const App = () => {
+  // store greeting in local state
+  const [greeting, setGreetingValue] = useState();
+
+  // request access to user's MetaMask account
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  };
+
+  // call Greeter smart contract, read the current greeting value
+  const setGreeting = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const web3provider = new ethers.BrowserProvider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        Greeter.abi,
+        web3provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log("data: ", data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  };
+
+  // call the smart contract to set new Greeting update
+  const getGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const web3provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await web3provider.getSigner();
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        Greeter.abi,
+        signer
+      );
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      setGreetingValue(" ");
+      console.log(greeting);
+      getGreeting();
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <button onClick={getGreeting}>Fetch Greeting</button> <br /> <hr />
+      <button onClick={setGreeting}>Set Greeting</button> <br /> <hr />
+      <input
+        onChange={(e) => {
+          //   console.log(e.target.value);
+          setGreetingValue(e.target.value);
+        }}
+        placeholder="Set greeting here..."
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
